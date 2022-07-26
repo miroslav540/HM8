20426cbbfb31975592a1b52192e06724b5f6bb4f
Miroslav <mir19972905@mai.ru>
2022-07-26 23:56:17 +0300

    task66

---
+++ task66/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: "); int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите последнее число: "); int N = Convert.ToInt32(Console.ReadLine());
+int SumNumbers(int M, int N)
+{
+    if (M == N) return M;
+    return (M + SumNumbers(M + 1, N));
+}
+Console.WriteLine(SumNumbers(M, N));
