37a193652b2eb705614f573c02872080b66d45e2
Miroslav <mir19972905@mai.ru>
2022-07-26 23:56:07 +0300

    task64

---
+++ task64/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: "); int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите последнее число: "); int N = Convert.ToInt32(Console.ReadLine());
+string PrintNumbers(int start, int end)
+{
+    if (start == end) return start.ToString();
+    return (start + ", " + PrintNumbers(start + 1, end));
+}
+Console.WriteLine(PrintNumbers(M, N));
