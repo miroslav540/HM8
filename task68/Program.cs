0d3449cdfb5335c01d8c01b75343549a021f80aa
Miroslav <mir19972905@mai.ru>
2022-07-26 23:57:05 +0300

    task6123

---
+++ task68/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int recursion(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    } 
+    else if (n == 0 && m > 0)
+    {
+        return recursion(m - 1, 1);
+    } 
+    else
+    {
+        return recursion(m - 1, recursion(m, n - 1));
+    }
+}
+void main(String[] args)
+{
+    Console.WriteLine(recursion(3, 2)); 
+}
+
+
+main(args);
